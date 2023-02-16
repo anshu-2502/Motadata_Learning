843e50e0b78489dfc8ef08f4801e82c888ea91af
anshu-2502 <anshusharma@jklu.edu.in>
2023-02-16 14:01:10 +0530

    access modifiers and static block

--- CoreJava/src/Feb16/AccessModifiers.java
+++ CoreJava/src/Feb16/AccessModifiers.java
@@ -1,7 +1,60 @@
 package Feb16;
 
+class BaseClass{
+
+//    void display(){
+//
+//        System.out.println("BaseClass:- default modifier ");
+//
+//    }
+
+//    public void display(){
+//
+//        System.out.println("BaseClass:- public  ");
+//
+//    }
+
+    protected void display(){
+
+        System.out.println("protected: base class");
+
+    }
+
+//    private void display(){
+//
+//        System.out.println("private :base class");
+//
+//    }
+
+}
+
+class DerivedClass extends BaseClass{
+
+
+}
+
+class DerivedderivedClass extends  DerivedClass{
+
+}
 public class AccessModifiers {
+
     public static void main(String[]args){
+
+
+        BaseClass BaseClassObject = new BaseClass();
+
+        DerivedClass derivedClassObject = new DerivedClass();
+
+        DerivedClass DerivedClassObject = new DerivedderivedClass();
+
+//        derivedClassObject.display();
+
+        DerivedClassObject.display();
+
+
+
+
+
 
 
 
