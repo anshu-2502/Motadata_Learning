df0477571adb1ea39f9f1fc43ceec6ff36766427
anshu-2502 <anshusharma@jklu.edu.in>
2023-02-23 23:14:29 +0530

    stack

--- CoreJava/src/AdvanceDSA_Lists_22Feb/Stack/stacks_array.java
+++ CoreJava/src/AdvanceDSA_Lists_22Feb/Stack/stacks_array.java
@@ -1,6 +1,7 @@
 package AdvanceDSA_Lists_22Feb.Stack;
 
 import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 public class stacks_array {
     protected int arr[];
@@ -31,8 +32,11 @@ public class stacks_array {
 
     public int peek()
     {
-        if( isEmpty() )
+        if( isEmpty() ) {
+
             throw new NoSuchElementException("Underflow Exception");
+
+        }
         return arr[top];
     }
 
@@ -44,12 +48,16 @@ public class stacks_array {
             arr[++top] = i;
         len++ ;
     }
-    /*  Function to delete an element from the stack */
+
     public int pop()
     {
-        if( isEmpty() )
+        if( isEmpty() ) {
+
             throw new NoSuchElementException("Underflow Exception");
+
+        }
         len-- ;
+
         return arr[top--];
     }
 
@@ -74,6 +82,75 @@ public class stacks_array {
 
 
 
-    }
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Stack Test\n");
+        System.out.println("Enter Size of Integer Stack ");
+        int n = scan.nextInt();
 
+        stacks_array stk = new stacks_array(n);
+
+        char ch;
+        do{
+            System.out.println("\nStack Operations");
+            System.out.println("1. push");
+            System.out.println("2. pop");
+            System.out.println("3. peek");
+            System.out.println("4. check empty");
+            System.out.println("5. check full");
+            System.out.println("6. size");
+            int choice = scan.nextInt();
+            switch (choice)
+            {
+                case 1 :
+                    System.out.println("Enter integer element to push");
+                    try
+                    {
+                        stk.push( scan.nextInt() );
+                    }
+                    catch (Exception e)
+                    {
+                        System.out.println("Error : " + e.getMessage());
+                    }
+                    break;
+                case 2 :
+                    try
+                    {
+                        System.out.println("Popped Element = " + stk.pop());
+                    }
+                    catch (Exception e)
+                    {
+                        System.out.println("Error : " + e.getMessage());
+                    }
+                    break;
+                case 3 :
+                    try
+                    {
+                        System.out.println("Peek Element = " + stk.peek());
+                    }
+                    catch (Exception e)
+                    {
+                        System.out.println("Error : " + e.getMessage());
+                    }
+                    break;
+                case 4 :
+                    System.out.println("Empty status = " + stk.isEmpty());
+                    break;
+                case 5 :
+                    System.out.println("Full status = " + stk.isFull());
+                    break;
+                case 6 :
+                    System.out.println("Size = " + stk.getSize());
+                    break;
+                default :
+                    System.out.println("Wrong Entry \n ");
+                    break;
+            }
+            /* display stack */
+            stk.display();
+            System.out.println("\nDo you want to continue (Type y or n) \n");
+            ch = scan.next().charAt(0);
+
+        } while (ch == 'Y'|| ch == 'y');
+    }
 }
+
